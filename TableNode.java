ceb5b2de7c48911234d1e35af1e0be65433702b9
12BrownBananas <garrettrossthompson@outlook.com>
2016-11-12 16:10:43 -0500

    Update TableNode.java

--- TableNode.java
+++ TableNode.java
@@ -3,9 +3,17 @@ public class TableNode {
 	Order myOrder;
 	String nodeID;
 	int tableStatus;
-	TableNode(String uniqueID) {
+	int x;
+	int y;
+	int genNotif; //However many unacknowledged general notifications we have
+	int refillNotif;
+	TableNode(String uniqueID, int xPos, int yPos) {
 		nodeID = uniqueID;
 		tableStatus = 0;
+		x = xPos;
+		y = yPos;
+		genNotif = 0;
+		refillNotif = 0;
 	}
 	public void createOrder() {
 		myOrder = new Order();
@@ -32,5 +40,21 @@ public class TableNode {
 	}
 	public int getStatus() {
 		return tableStatus;
+	}
+	public void modifyXPos(int newX) {
+		x = newX;
+	}
+	public void modifyYPos(int newY) {
+		y = newY;
+	}
+	public void incrementGenNotif() {
+		genNotif++;
+	}
+	public void incrementRefillNotif() {
+		refillNotif++;
+	}
+	public void acknowledgeNotif() {
+		refillNotif = 0;
+		genNotif = 0;
 	}
 }
