18c2ea7dff45aa1299c76b0d88cb8445b1a1d026
12BrownBananas <garrettrossthompson@outlook.com>
2016-11-24 17:12:46 -0500

    Add files via upload

--- TableNode.java
+++ TableNode.java
@@ -1,30 +1,68 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class TableNode extends JPanel  {
+public class TableNode  {
 	Order myOrder;
 	String nodeID;
 	int tableStatus;
 	int x;
 	int y;
+	int oldX;
+	int oldY;
+	int xOffset;
+	int yOffset; //Offset value for drawing properly
+	int selectYOffset;
 	int genNotif; //However many unacknowledged general notifications we have
 	int refillNotif;
-	ImageIcon nodeIcon;
+	BufferedImage nodeIcon;
+	BufferedImage dialogBox;
+	BufferedImage trashCan;
+	BufferedImage button;
+	BufferedImage button2;
+	int buttonX;
+	int buttonY;
+	int dialogBoxX;
+	int dialogBoxY; //Both of these are relative to the x and y coordinate of the node
+	int trashX;
+	int trashY;
+	boolean isSelected;
 	LayoutView view;
 	TableNode(String uniqueID, int xPos, int yPos, LayoutView myView) {
+		view = myView;
 		nodeID = uniqueID;
 		tableStatus = 0;
+		isSelected = false;
 		x = xPos;
 		y = yPos;
+		oldX = x;
+		oldY = y;
+		xOffset = -6;
+		yOffset = -22;
+		selectYOffset = 32;
 		genNotif = 0;
 		refillNotif = 0;
-		nodeIcon = new ImageIcon("node0.png");
-		view = myView;
-	}
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		nodeIcon.paintIcon(view, g, (int)x, (int)y);
+		try {
+			nodeIcon = ImageIO.read(new File("node0_1.png"));
+			dialogBox = ImageIO.read(new File("nodeDialogBox.png"));
+			trashCan = ImageIO.read(new File("trash.png"));
+			button = ImageIO.read(new File("button0.png"));
+			button2 = ImageIO.read(new File("button1.png"));
+		}
+		catch (IOException e) {
+		}
+		dialogBoxX = nodeIcon.getWidth();
+		dialogBoxY = nodeIcon.getHeight()+yOffset*3;
+		trashX = dialogBoxX+dialogBox.getWidth()/3+dialogBox.getWidth()/28;
+		trashY = dialogBoxY+dialogBox.getHeight()+yOffset*5;
+		buttonX = dialogBoxX+dialogBox.getWidth()/5;
+		buttonY = dialogBoxY-yOffset;
 	}
 	public void createOrder() {
 		myOrder = new Order();
