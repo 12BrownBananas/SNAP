08f286e1e38755939f7a5e1576bdf3693c3db28a
spurgeonbush <spurgeonbush@gmail.com>
2016-11-29 00:13:03 -0500

    Update NetworkClient.java

--- NetworkClient.java
+++ NetworkClient.java
@@ -1,24 +1,17 @@
 //"When prompted  for the IP adress enter "localhost". this is for testing only
 // and enter a table name or ID
 import java.awt.Button;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -31,19 +24,11 @@ public class NetworkClient {
     JTextArea messageArea = new JTextArea(8, 40);
 	Button ServiceButton= new Button("Touch Me for Service");
 	ClientController myController;
+	ArrayList <Integer> TempNodes = new ArrayList<Integer>();
+	private ObjectOutputStream outPut;
+	private ObjectInputStream inPut;
 	
     public NetworkClient() throws IOException {
-    }
-
-    /**
-     * Prompt for and return the address of the server.
-     */
-    private String getServerAddress() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "",
-            JOptionPane.QUESTION_MESSAGE);
     }
 
     /**
@@ -64,14 +49,22 @@ public class NetworkClient {
     private void run() throws IOException, InterruptedException {
 
         // Make connection and initialize streams
-        String serverAddress = getServerAddress();
+        String serverAddress = "localhost";
         @SuppressWarnings("resource")
 		Socket socket = new Socket(serverAddress, 9003);
         in = new BufferedReader(new InputStreamReader(
             socket.getInputStream()));
         out = new PrintWriter(socket.getOutputStream(), true);
         // Process all messages from server
+        
+        outPut = new ObjectOutputStream(socket.getOutputStream());
+        inPut = new ObjectInputStream(socket.getInputStream());
+        
+        
+        
+        
         while (true) {
+        	ObjectOutputStream outPut = new ObjectOutputStream(socket.getOutputStream());
             String line = in.readLine();
             if (line.startsWith("NAME")) {
                 String id = getName();
@@ -79,6 +72,12 @@ public class NetworkClient {
                 ClientView view = new ClientView();
                 ClientController ctrl = new ClientController(id, this, view);
                 myController = ctrl;
+                while(myController.synched=true){
+                TempNodes.add(myController.myNode.tableStatus);
+                outPut.writeObject(TempNodes);
+                //System.out.print(myController.myNode.tableStatus);
+                }
+              
                 //I want to be able to send myController.myNode to the server at any time
                 ctrl.repaint();
             } else if (line.startsWith("ACCEPTED")) {
