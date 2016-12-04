233df198e61270ab8d4da4e25333c9d2fbe1462c
12BrownBananas <garrettrossthompson@outlook.com>
2016-12-04 14:00:43 -0500

    Update LayoutController.java

--- LayoutController.java
+++ LayoutController.java
@@ -36,6 +36,15 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 	ArrayList<ClientController> myClients;
 	
 	NetworkServer myNetwork;
+	/*
+	* @author Garrett Thompson
+	* Constructor method for LayoutController. The second component of the
+	* server-side interface in creation order. Creates its own view.
+	* Initializes variables to keep track of its state. Adds listeners to
+	* its view such that user input can be accepted. 
+	* @throws IOException
+	* @throws InterruptedException
+	*/
 	public LayoutController() throws IOException, InterruptedException {
 		//network = ntwk;
 		view = new LayoutView(this);
@@ -51,7 +60,14 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		layout = ImageIO.read(new File("floorplan.png")); //Placeholder floorplan pulled from http://evstudio.com/giovanni-italian-restaurant-floor-plans/
 		repaint();
 	}
-	
+	/*
+	* @author Garrett Thompson
+	* Takes a given TableNode object already stored in the LayoutController and 
+	* updates its relevant internal variables to be equivalent to the supplied model node.
+	* Used when a node object is sent through the network to be synchronized with its corresponding
+	* server-side node.
+	* @param node  The model node in question.
+	*/
 	public void updateNode(TableNode node) {
 		for (int i = 0; i < nodes.size(); i++) {
 			if (nodes.get(i).nodeID.equals(node.nodeID)) {
@@ -63,6 +79,12 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
+	/*
+	* @author Garrett Thompson
+	* Checks the network to see if the supplied node ID corresponds to any existing nodes in its
+	* registry of client-side table nodes. Presently doesn't do anything.
+	* @param id  The nodeID of the node to check for.
+	*/
 	private boolean isNodeInServer(String id) {
 		String node = null;
 		//node = network.findNode(id);
@@ -71,7 +93,14 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return false;
 	}
-
+	/* 
+	* @author Garrett Thompson
+	* An override of the MouseListener's mouseClicked event. Called every time the user
+	* inputs a mouse click (press, no movement, release). At the moment, used to interact
+	* with two node dialog box buttons: trash and advance. Trash deletes a node, whereas
+	* advance moves the node's status forward.
+	* @param e  The mouse event.
+	*/
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (isOverDialogBox(e.getX(), e.getY())) {
@@ -95,6 +124,15 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
+	/*
+	* @author Garrett Thompson
+	* Checks to see if the mouse is hovering over the advance node button, which is located in 
+	* the table node dialog box, but only if the node has been synched with the server. Useful for seeing
+	* if the advance button is being selected or not.
+	* @param x   X coordinate of the mouse.
+	* @param y   Y coordinate of the mouse.
+	* @param node  The table node whose advance button we're checking to see if we're hovering over.
+	*/
 	public boolean isOverAdvance(int x, int y, TableNode node) {
 		if (node.isSelected) {
 			int leftX = node.x+node.buttonX;
@@ -109,16 +147,34 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return false;
 	}
+	/* 
+	* Arbitrary mouseListener event stub override required by Java.
+	*/
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/* 
+	* Arbitrary mouseListener event stub override required by Java.
+	*/
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	/*
+	* @author Garrett Thompson
+	* Override of the mousePressed method in MouseListener. Used primarily for preparing to select
+	* GUI buttons (the actual selection doesn't occur until mouseReleased) and generating nodes on
+	* the layout. 
+	*
+	* For further extensions of this code, I recommend placing the node generation code found here
+	* in its own separate method.
+	*
+	* @param e  The mouse pressed event.
+	*/
 	@Override
 	public void mousePressed(MouseEvent e) {
 		
@@ -216,6 +272,13 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
+	/*
+	* @author Garrett Thompson
+	* An override of the MouseListener mouseReleased event which is used primarily for selecting
+	* buttons once they've been pressed, setting the position of nodes on the layout, and selecting and deselecting
+	* nodes.
+	* @param e  The mouse released event.
+	*/
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		if (SwingUtilities.isRightMouseButton(e)) {
@@ -279,7 +342,14 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
-	
+	/*
+	* @author Garrett Thompson
+	* Similar to the isOverAdvance method, this checks to see if the mouse is over the trash button
+	* on the table node dialog box. Useful for checking to see if that button should be selected or not.
+	* @param x  X coordinate of the mouse.
+	* @param y  Y coordinate of the mouse.
+	* @param node  Table Node object whose button we're checking against.
+	*/
 	public boolean isOverTrash(int x, int y, TableNode node) {
 		if (node.synched == false) {
 			if (x > node.x+node.trashX && x < node.x+node.trashX+node.trashCan.getWidth()) {
@@ -290,6 +360,18 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return false;
 	}
+	
+	/*
+	* @author Garrett Thompson
+	* Checks to see if one node (our "first" node) is colliding with another ("second" node), for example, by sharing the same space.
+	* Formerly used to make sure that nodes would not overlap on the layout, this functionality was
+	* removed later on in development after user testing revealed it was irritating and unhelpful.
+	* @param x1  Leftmost coordinate of the first node.
+	* @param y1 Topmost coordinate of the first node.
+	* @param x2  Rightmost coordinate of the first node.
+	* @param y2  Bottommost coordinate of the first node.
+	* @param node  The first node object, whose bounding box is defined by the four previously-given coordinates.
+	*/
 	
 	public boolean collisionWithNode(int x1, int y1, int x2, int y2, TableNode node) { //The node is the node that we DON'T check against
 		if (x1 > x2) {
@@ -302,17 +384,26 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			y2 = y1;
 			y1 = temp;
 		}
-		for (int k = 0; k < nodes.size(); k++) {
-			if (nodes.get(k).nodeID != node.nodeID) {
+		for (int k = 0; k < nodes.size(); k++) { //Checks every node
+			if (nodes.get(k).nodeID != node.nodeID) { //As long as the node we're checking against is NOT the first node.
 				if ((nodes.get(k).x > x1 && nodes.get(k).x < x2) || (nodes.get(k).x+(x2-x1) < x2 && nodes.get(k).x+(x2-x1) >= x1) || (nodes.get(k).x == x1)) {
 					if ((nodes.get(k).y > y1 && nodes.get(k).y < y2) || (nodes.get(k).y+(y2-y1) < y2 && nodes.get(k).y+(y2-y1) > y1) || nodes.get(k).y == y1) {
-						return true;
+						return true; //We reach this point in code if any of the four coordinates given to this method overlap with 
+							//any of the bounding boxes of other ("second") nodes.
 					}
 				}
 			}
 		}
 		return false;
 	}
+	
+	/*
+	* @author Garrett Thompson
+	* Checks to see if the mouse is hovering over a node, which is used to determine if a node is selectable.
+	* @param x  X coordinate of the mouse.
+	* @param y  Y coordinate of the mouse.
+	*/
+	
 	private boolean isOverNode(int x, int y) {
 		for (int k = 0; k < nodes.size(); k++) {
 			if ((nodes.get(k).x < x && nodes.get(k).x+nodes.get(k).nodeIcon.getWidth() > x)) {
@@ -323,6 +414,13 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return false;
 	}
+	/* 
+	* @author Garrett Thompson
+	* If we are over a node, it's useful to know which node object specifically we're hovering over with the mouse.
+	* This method returns the reference to the node object which the mouse is currently over.
+	* @param x  X coordinate of the mouse.
+	* @param y  Y coordinate of the mouse.
+	*/
 	private TableNode nodeOver(int x, int y) {
 		for (int k = 0; k < nodes.size(); k++) {
 			if ((nodes.get(k).x < x && nodes.get(k).x+nodes.get(k).nodeIcon.getWidth() > x)) {
@@ -333,6 +431,13 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return null;
 	}
+	/*
+	* @author Garrett Thompson
+	* Checks to see if the mouse is over a node dialog box. Can only happen if a node has been selected, thus
+	* causing its dialog box to be visible. Useful for seeing if node buttons can be pressed.
+	* @param x  X coordinate of the mouse.
+	* @param y  Y coordinate of the mouse.
+	*/
 	private boolean isOverDialogBox(int x, int y) {
 		for (int i = 0; i < nodes.size(); i++) {
 			if (nodes.get(i).isSelected) {
@@ -345,7 +450,14 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		}
 		return false;
 	}
-	
+	/*
+	* @author Garrett Thompson
+	* Checks to see if the mouse is over the main button (sync node or view order, depending on sync status)
+	* of a node's dialog box. Used to see if the main button is being clicked.
+	* @param x  X coordinate of the mouse
+	* @param y  Y coordinate of the mouse
+	* @param node  Table node we're checking against.
+	*/
 	private boolean isOverButton(int x, int y, TableNode node) {
 		if (x > node.x+node.buttonX && x < node.x+node.buttonX+node.button.getWidth()) {
 			if (y > node.y+node.buttonY+node.selectYOffset && y < node.y+node.buttonY+node.button.getHeight()+node.selectYOffset) {
@@ -355,7 +467,12 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 		return false;
 	}
 	
-	
+	/* 
+	* @author Garrett Thompson
+	* This method dictates how the states of this object are represented graphically. This component is
+	* later drawn to the screen in the LayoutView object.
+	* @param g  The graphics context for drawing.
+	*/
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -366,7 +483,7 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 				g.drawImage(nodes.get(i).notifIcon, nodes.get(i).x, nodes.get(i).y,null);
 			}
 		}
-		for (int i = 0; i < nodes.size(); i++) {
+		for (int i = 0; i < nodes.size(); i++) { //We're going to iterate through every node in creation order and draw them at their respective coordinates in the same way.
 			if (nodes.get(i).isSelected) {
 				g.drawImage(nodes.get(i).dialogBox, nodes.get(i).x+nodes.get(i).dialogBoxX, nodes.get(i).y+nodes.get(i).dialogBoxY, null);
 				g.drawString("#"+nodes.get(i).nodeID, nodes.get(i).x+nodes.get(i).dialogBoxX-nodes.get(i).xOffset*10, nodes.get(i).y+nodes.get(i).dialogBoxY-nodes.get(i).yOffset-2);
@@ -387,7 +504,12 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
-
+	/*
+	* @author Garrett Thompson
+	* This override is mainly for quality of life purposes-- if the mouse is dragged off of a button before we select that button, 
+	* the button is considered deselected and will not be activated when the mouse is released.
+	* @param arg0  The mouse event.
+	*/
 	@Override
 	public void mouseDragged(MouseEvent arg0) {
 		if (isHoldingNode) {
@@ -414,23 +536,37 @@ public class LayoutController extends JPanel implements MouseListener, MouseMoti
 			}
 		}
 	}
-
+	
+	/* 
+	* Arbitrary MouseMovementListener event stub override required by Java.
+	*/
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
 		
 	}
 
+	/* 
+	* Arbitrary KeyListener event stub override required by Java.
+	*/
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 
 		
 	}
 
+	/* 
+	* Arbitrary KeyListener event stub override required by Java.
+	*/
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
+	/*
+	* @author Garrett Thompson
+	* Creates a new client every time a key is pressed. Used for debugging client-side interface. Should be removed
+	* for final build.
+	*/
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
