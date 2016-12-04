d20466f8dce7e8f75854e8cce0e2f3be4a9402c3
12BrownBananas <garrettrossthompson@outlook.com>
2016-12-04 15:54:16 -0500

    Update TableNode.java

--- TableNode.java
+++ TableNode.java
@@ -9,7 +9,15 @@ import java.io.Serializable;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-
+/*
+* @author Garrett Thompson
+* Created: November 4th
+* Last Edited: November 23rd
+* TableNode.java
+* A fundamental class for SNAP, the Table Node object is used by both the client and server components
+* to keep track of the current status of tables. 
+* Expected Revisions: Negligible
+*/
 public class TableNode implements Serializable {
 	tableOrder thisOrder;
 	String nodeID;
@@ -29,7 +37,7 @@ public class TableNode implements Serializable {
 	BufferedImage dialogBox;
 	BufferedImage trashCan;
 	BufferedImage button;
-	BufferedImage button2;
+	BufferedImage button2; //The "pressed" state of button
 	BufferedImage orderButton;
 	BufferedImage notifIcon;
 	BufferedImage advanceIcon;
@@ -47,6 +55,14 @@ public class TableNode implements Serializable {
 	tableOrder tableOrder;
 	
 	Ellipse2D advanceButtonMask;
+	
+	/*
+	* @author Garrett Thompson
+	* Client-side constructor for the object. Some details are left blank due to client-side operations making them
+	* irrelevant.
+	* @param uniqueID  A unique node identification string (four-digit) such that the node will be uniquely present in the network.
+	* @param myController  Controller object this node is associated with.
+	*/
 	TableNode(String uniqueID, ClientController myController) {
 		ctrl = myController;
 		isClientSide = true;
@@ -60,6 +76,15 @@ public class TableNode implements Serializable {
 		statusNames[4] = "Order Delivered";
 		statusNames[5] = "Ready to Pay";
 	}
+	/*
+	*@author Garrett Thompson
+	* Server-side constructor for the object. Introduces new variables to account for the extended
+	* node-specific features of the server-side interface.
+	* @param uniqueID  A unique four-digit identification string for the node to ensure that it is unique on the network
+	* @param xPos  X coordinate for the node's location
+	* @param yPos  Y coordinate for the node's location
+	* @param myView  LayoutView object this node will be drawn by. I don't think this is being used in our current build but I don't want to break anything by taking it out.
+	*/
 	TableNode(String uniqueID, int xPos, int yPos, LayoutView myView) throws NumberFormatException, IOException {
 		view = myView;
 		nodeID = uniqueID;
@@ -99,12 +124,21 @@ public class TableNode implements Serializable {
 		buttonY = dialogBoxY-yOffset;
 		//advanceButtonMask = new Ellipse2D.Double(x+buttonX, y+buttonY+orderButton.getHeight()+16, advanceIcon.getWidth(), advanceIcon.getHeight());
 	}
+	/* @author Garrett Thompson
+	* Generates a new empty order to be stored in the table node. 
+	*/
 	public void createOrder() {
 		thisOrder = new tableOrder(nodeID);
 	}
+	/* @author Garrett Thompson
+	* Removes the order currently stored in the node
+	*/
 	public void removeOrder() {
 		thisOrder = null;
 	}
+	/* @author Garrett Thompson
+	* Advances node status between values 0 and 5, to represent each possible status of the node.
+	*/
 	public void advanceStatus() {
 		if (tableStatus < 5) {
 			tableStatus++;
@@ -117,6 +151,9 @@ public class TableNode implements Serializable {
 			view.myController.repaint();
 		}
 	}
+	/* @author Garrett Thompson
+	* Updates image of node based on current node status.
+	*/
 	public void updateNodeImage() {
 		File imgFile = null;
 		if (tableStatus == 0) {
@@ -147,6 +184,9 @@ public class TableNode implements Serializable {
 			e.printStackTrace();
 		}
 	}
+	/* @author Garrett Thompson
+	* Basically the reverse of advanceStatus(). Steps back status by one, unless it's already at zero.
+	*/
 	public void stepBackStatus() {
 		if (tableStatus > 0) {
 			tableStatus--;
@@ -154,27 +194,50 @@ public class TableNode implements Serializable {
 		updateNodeImage();
 		view.myController.repaint();
 	}
+	/* @author Garrett Thompson
+	* Sets table status back to its initial state, zero.
+	*/
 	public void resetStatus() {
 		tableStatus = 0;
 		tableOrder = null;
 		updateNodeImage();
 		view.myController.repaint();
 	}
+	/* @author Garrett Thompson
+	* @returns Current table status
+	*/
 	public int getStatus() {
 		return tableStatus;
 	}
+	/* @author Garrett Thompson
+	* Changes x position based on paramter.
+	* @param newX  the x coordinate to change this node's x position to.
+	*/
 	public void modifyXPos(int newX) {
 		x = newX;
 	}
+	/* @author Garrett Thompson
+	* Changes y position based on parameter.
+	* @param newY  the y coordinate to change this node's x position to.
+	*/
 	public void modifyYPos(int newY) {
 		y = newY;
 	}
+	/* @author Garrett Thompson
+	* Increments the tally of general notifications sent to/from the table node.
+	*/
 	public void incrementGenNotif() {
 		genNotif++;
 	}
+	/* @author Garrett Thompson
+	* Increments the tally of refill notifications sent to/from this table node. Not currently implemented.
+	*/
 	public void incrementRefillNotif() {
 		refillNotif++;
 	}
+	/* @author Garrett Thompson
+	* Reverts notifications to zero, thus "acknowledging" them.
+	*/
 	public void acknowledgeNotif() {
 		refillNotif = 0;
 		genNotif = 0;
