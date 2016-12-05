426dd4a145155a69021b9d4ad23ac9cea7ca7c61
12BrownBananas <garrettrossthompson@outlook.com>
2016-12-04 20:54:41 -0500

    Update ClientView.java

--- ClientView.java
+++ ClientView.java
@@ -9,7 +9,14 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
-
+/*
+* @author Garrett Thompson
+* Created: November 16th
+* Last Edited: November 24th
+* ClientView.java
+* View object for client interface. Creates a viewing window (JPanel) to draw client-side GUI on.
+* Expected Revisions: Support for adjustible resolution
+*/ 
 public class ClientView extends JPanel implements ImageObserver {
 	int height;
 	int width;
@@ -18,10 +25,13 @@ public class ClientView extends JPanel implements ImageObserver {
 	private JLayeredPane lpane = new JLayeredPane();
 	Color alpha;
 	ClientController myController;
+	/* @author Garrett Thompson
+	* Constructor method. Initializes the size of the viewing frame and creates the frame.
+	*/
 	public ClientView() throws IOException, InterruptedException {
 		alpha = new Color(1f, 0f, 0f, 0f);
 		height = 720;
-		width = 960;
+		width = 960; //4:3 aspect ratio, 
 		running = true;
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.getContentPane().setLayout(new BorderLayout());
@@ -30,6 +40,9 @@ public class ClientView extends JPanel implements ImageObserver {
 		frame.pack();
 		frame.setVisible(true);
 	}
+	/* @author Garrett Thompson, even though I didn't really do anything.
+	* This method makes this class paint itself.
+	*/
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	}
