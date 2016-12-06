4ed902fb09ae6a48c78529a9ef82a0637fca73be
12BrownBananas <garrettrossthompson@outlook.com>
2016-12-06 10:55:21 -0500

    Update SnapMain.java

--- SnapMain.java
+++ SnapMain.java
@@ -3,8 +3,20 @@ import java.net.ServerSocket;
 
 import javax.swing.JFrame;
 
+/*
+* @author Garrett Thompson
+* Created: November 26th
+* Last Edited: November 27th
+* SnapMain.java
+* Runs the program. Would initialize only the server-side initially but due to our current 
+* lack of a network component, is currently used to create both the client and server.
+* Expected Revisions: Negligible
+*/ 
 public class SnapMain {
 	private static final int PORT = 9003;
+	/* @author Garrett Thompson
+	*  Main method. Begins running the program.
+	*/
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		LayoutController controller = new LayoutController();
